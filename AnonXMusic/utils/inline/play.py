342cdbfe32a52ea277de4622e2ae14389d8e54be
Visionx-x <telegrambott19@gmail.com>
2025-12-21 13:13:01 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -1,6 +1,7 @@
 import math
-from AnonXMusic import app  # Make sure BOT_USERNAME is set at startup
+
 from pyrogram.types import InlineKeyboardButton
+
 from AnonXMusic.utils.formatters import time_to_seconds
 
 
@@ -26,33 +27,31 @@ def track_markup(_, videoid, user_id, channel, fplay):
     return buttons
 
 
-def stream_markup_timer(_, videoid, user_id, chat_id, played, dur):
+def stream_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
-
     if 0 < umm <= 10:
-        bar = "▬▭▭▭▭▭▭▭▭▭"
+        bar = "◉—————————"
     elif 10 < umm < 20:
-        bar = "▬▬▭▭▭▭▭▭▭▭"
+        bar = "—◉————————"
     elif 20 <= umm < 30:
-        bar = "▬▬▬▭▭▭▭▭▭▭"
+        bar = "——◉———————"
     elif 30 <= umm < 40:
-        bar = "▬▬▬▬▭▭▭▭▭▭"
+        bar = "———◉——————"
     elif 40 <= umm < 50:
-        bar = "▬▬▬▬▬▭▭▭▭▭"
+        bar = "————◉—————"
     elif 50 <= umm < 60:
-        bar = "▬▬▬▬▬▬▭▭▭▭"
+        bar = "—————◉————"
     elif 60 <= umm < 70:
-        bar = "▬▬▬▬▬▬▬▭▭▭"
+        bar = "——————◉———"
     elif 70 <= umm < 80:
-        bar = "▬▬▬▬▬▬▬▬▭▭"
+        bar = "———————◉——"
     elif 80 <= umm < 95:
-        bar = "▬▬▬▬▬▬▬▬▬▭"
+        bar = "————————◉—"
     else:
-        bar = "▬▬▬▬▬▬▬▬▬▬"
-
+        bar = "—————————◉"
     buttons = [
         [
             InlineKeyboardButton(
@@ -60,31 +59,24 @@ def stream_markup_timer(_, videoid, user_id, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [
+        [   
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
-            ),
-            InlineKeyboardButton(
-                text="➕ Add Me",
-                url=f"https://t.me/Rose_MusicsBot?startgroup=true"
-            )
-        ],
     ]
+    
     return buttons
 
 
-def stream_markup(_, videoid, user_id, chat_id):
+def stream_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
@@ -118,6 +110,10 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
                 callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="➕ Add Me",
+                url=f"https://t.me/Rose_MusicsBot?startgroup=true"
             )
         ],
     ]
@@ -130,6 +126,12 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
             InlineKeyboardButton(
                 text=_["P_B_3"],
                 callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
     ]
