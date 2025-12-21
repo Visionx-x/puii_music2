d5a0e245bb4197b7c0cb0b63c1734860704aa392
Visionx-x <telegrambott19@gmail.com>
2025-12-21 13:04:26 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -1,7 +1,6 @@
 import math
-
+from AnonXMusic import app  # Make sure BOT_USERNAME is set at startup
 from pyrogram.types import InlineKeyboardButton
-
 from AnonXMusic.utils.formatters import time_to_seconds
 
 
@@ -27,31 +26,33 @@ def track_markup(_, videoid, user_id, channel, fplay):
     return buttons
 
 
-def stream_markup_timer(_, chat_id, played, dur):
+def stream_markup_timer(_, videoid, user_id, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
+
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "▬▭▭▭▭▭▭▭▭▭"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "▬▬▭▭▭▭▭▭▭▭"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "▬▬▬▭▭▭▭▭▭▭"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "▬▬▬▬▭▭▭▭▭▭"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "▬▬▬▬▬▭▭▭▭▭"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "▬▬▬▬▬▬▭▭▭▭"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "▬▬▬▬▬▬▬▭▭▭"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "▬▬▬▬▬▬▬▬▭▭"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "▬▬▬▬▬▬▬▬▬▭"
     else:
-        bar = "—————————◉"
+        bar = "▬▬▬▬▬▬▬▬▬▬"
+
     buttons = [
         [
             InlineKeyboardButton(
@@ -59,26 +60,43 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [   
+        [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="➕ Add Me",
+                url=f"https://t.me/{app.username}?startgroup=true"
+            )
+        ],
     ]
-    
     return buttons
 
 
-def stream_markup(_, chat_id):
+def stream_markup(_, videoid, user_id, chat_id):
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="➕ Add Me",
+                url=f"https://t.me/{app.username}?startgroup=true"
+            )
         ],
     ]
     return buttons
@@ -100,7 +118,7 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
                 callback_data=f"forceclose {videoid}|{user_id}",
-            ),
+            )
         ],
     ]
     return buttons
@@ -112,12 +130,6 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
             InlineKeyboardButton(
                 text=_["P_B_3"],
                 callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
     ]
