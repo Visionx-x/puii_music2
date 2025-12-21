eb89b384d31907018e6a5c83c7855b1ef590d4e8
Visionx-x <telegrambott19@gmail.com>
2025-12-21 11:10:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+YTPROXY_URL = getenv("YTPROXY_URL", 'http://213.199.39.92:1470') ## xBit Music Endpoint.
+YT_API_KEY = getenv("YT_API_KEY" , '1a873582a7c83342f961xx0a177b2b26' ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
 
 ## Other vaes
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
